b4136fcda71eb76a3dc7446f02ad5e090c547dd6
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-08 13:21:16 +0330

    change slug service provider to url

---
+++ src/UrlServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace JobMetric\Url;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class UrlServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-url')
+            ->hasConfig()
+            ->hasMigration();
+    }
+}
