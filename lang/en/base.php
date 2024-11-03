88ea4679d197fe4b2cfed7e97abb8a31de097a1c
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-03 22:10:19 +0330

    added some key in language file and add fa language file

--- lang/en/base.php
+++ lang/en/base.php
@@ -12,21 +12,33 @@ return [
     |
     */
 
-    'validation' => [
-        'errors' => 'Validation errors occurred.',
-        'not_found' => 'The url not found.',
+    "rule" => [
+        "exist" => "Url :field already exists.",
     ],
 
-    'messages' => [
-        'found' => 'The url was found successfully.',
-        'created' => 'The url was created successfully.',
-        'updated' => 'The url was updated successfully.',
-        'deleted' => 'The url was deleted successfully.',
+    "validation" => [
+        "errors" => "Validation errors occurred.",
+        "not_found" => "The url not found.",
     ],
 
-    'exceptions' => [
-        'not_found' => 'Url not found',
-        'url_too_long' => 'The url is too long.',
+    "messages" => [
+        "found" => "The url was found successfully.",
+        "created" => "The url was created successfully.",
+        "updated" => "The url was updated successfully.",
+        "deleted" => "The url was deleted successfully.",
+    ],
+
+    "exceptions" => [
+        "not_found" => "Url not found",
+        "url_too_long" => "The url is too long.",
+    ],
+
+    "fields" => [
+        "slug" => [
+            "title" => "Slug",
+            "placeholder" => "Enter the slug.",
+            "description" => "The 'slug' is a version of the name that is appropriate for a web address. It is usually all lowercase and contains only letters, numbers, and hyphens.",
+        ],
     ],
 
 ];
