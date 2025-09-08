da51bdf3189fa71cf3265d663f627317359a0f8a
majidmohammadian <majeedmohammadian@gmail.com>
2025-09-08 22:55:03 +0330

    Translation file cleanup changes

--- lang/en/base.php
+++ lang/en/base.php
@@ -16,18 +16,6 @@ return [
         "exist" => "Url already exists.",
     ],
 
-    "validation" => [
-        "errors" => "Validation errors occurred.",
-        "not_found" => "The url not found.",
-    ],
-
-    "messages" => [
-        "found" => "The url was found successfully.",
-        "created" => "The url was created successfully.",
-        "updated" => "The url was updated successfully.",
-        "deleted" => "The url was deleted successfully.",
-    ],
-
     "exceptions" => [
         "not_found" => "Url not found",
         "slug_not_found" => "The slug not found.",
